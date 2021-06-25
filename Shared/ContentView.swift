404f2035ebe8213ceca8597ae28daea5779a3a97
amir pirzad <pirzad7@gmail.com>
2021-06-25 19:22:01 +0430

    create project

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer().frame(height: 30)
+            HStack{
+                Spacer()
+                Text("MyTodoApp!").font(.largeTitle).bold()
+                Spacer()
+            }
+            Spacer()
+        }
+
     }
 }
 
