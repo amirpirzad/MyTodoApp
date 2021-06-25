daf917d5ca3769432aad315ad31357f605590415
amir pirzad <pirzad7@gmail.com>
2021-06-25 19:09:22 +0430

    Initial Commit

---
+++ Shared/MyTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoAppApp.swift
+//  Shared
+//
+//  Created by Pirzad on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
